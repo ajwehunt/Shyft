8d8123e0bb5480cd1c4d56203c648fb52faba497
ajwehunt <adamjwehunt@gmail.com>
2016-12-04 21:48:29 -0700

    gmapstoken

--- src/services/GetGoogleMap.js
+++ src/services/GetGoogleMap.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 var config = require('../../config');
 
 export function getLocation() {
-  return axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyC1Re0Gw6OIAb7Y5_Ax81XkqJ_htai7_Jo')
+  return axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDrzX2pFc5mTwnwwA2cqXKo2nLVV2Zkq5I')
 
 }
 
